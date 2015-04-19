5d62784aa713386df2903cc12e864b50e98fb303
Peter Hellberg <peter@c7.se>
2015-04-19 12:55:48 +0200

    Added initial API tests
     - expected constants
     - newInfoRequest
     - newDeleteRequest
     - newUploadRequest

---
+++ api/api_test.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+
+	"testing"
+)
+
+func TestAPI(t *testing.T) {
+	Convey("API", t, func() {
+		cred := &Credentials{"foo", "bar"}
+
+		Convey("expected constants", func() {
+			So(apiURL, ShouldEqual, "https://neocities.org/api/")
+			So(userAgent, ShouldEqual, "neocities (Go 1.4.2 package http)")
+		})
+
+		Convey("newInfoRequest", func() {
+			req, err := newInfoRequest(cred, "foo")
+
+			So(err, ShouldBeNil)
+			So(req.Method, ShouldEqual, "GET")
+			So(req.URL.String(), ShouldEqual, "https://neocities.org/api/info?sitename=foo")
+		})
+
+		Convey("newDeleteRequest", func() {
+			req, err := newDeleteRequest(cred, []string{"foo", "bar"})
+
+			So(err, ShouldBeNil)
+			So(req.Method, ShouldEqual, "POST")
+			So(req.URL.String(), ShouldEqual, "https://neocities.org/api/delete")
+		})
+
+		Convey("newUploadRequest", func() {
+			req, err := newUploadRequest(cred, []string{"../LICENSE"})
+
+			So(err, ShouldBeNil)
+			So(req.Method, ShouldEqual, "POST")
+			So(req.URL.String(), ShouldEqual, "https://neocities.org/api/upload")
+		})
+	})
+}
